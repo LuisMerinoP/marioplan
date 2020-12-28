e3b6d4bd7f58cd0e1afe89a92ea8d11c94daa65c
rustyBucketBay <merino.p.luis@gmail.com>
2020-12-28 21:26:39 +0100

    - selective routing - user sign up

--- src/components/auth/signUp.js
+++ src/components/auth/signUp.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signUp } from '../../store/actions/authActions'
 
-class signUp extends Component {
+class SignUp extends Component {
   state = {
     email: '',
     password: '',
@@ -16,10 +19,12 @@ class signUp extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signUp(this.state);
   }
 
   render() {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to='/' />
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -42,6 +47,9 @@ class signUp extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+            <div className="red-text center">
+              { authError ? <p>{ authError }</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -49,4 +57,17 @@ class signUp extends Component {
   }
 }
 
-export default signUp
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,4 +1,7 @@
-const initState = {}
+const initState = {
+  authError: null
+}
+
 
 const authReducer = (state = initState, action) => {
   switch (action.type) {
@@ -8,15 +11,32 @@ const authReducer = (state = initState, action) => {
         ...state, 
         authError: 'Login failed'
       }
+      
     case 'LOGIN_SUCCESS':
       console.log('login success')
       return {
         ...state,
         authError: null
       }
+
     case 'SIGNOUT_SUCCESS':
       console.log('signout success');
       return state;
+
+    case 'SIGNUP_SUCCESS':
+      console.log('signup success');
+      return {
+        ...state,
+        authError: null
+      }
+
+    case 'SIGNUP_ERROR':
+      console.log('signup error');
+      return {
+        ...state,
+        authError: action.err.message
+      }
+
     default:
       return state;
   }
